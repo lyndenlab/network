f8ab560fea8bd6c85c00a66fff37303110213cc9
lynden <lyndenlab@gmai.com>
2016-10-12 00:14:17 +0900

    add l4udp_server.py

--- connect.py
+++ connect.py
@@ -1,11 +1,12 @@
 import socket
 
-port = "1234"
+port = 1234
 ip = "192.168.11.31"
 
 skt = socket.socket()
 
 skt.connect((ip,port))
 skt.send("hello lain...")
-skt.recieved()
+message = skt.recieved()
+print (message)
 skt.close()
---
+++ l4udp_server.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python
+
+import socket
+import threading
+
+ipaddr = "0.0.0.0"
+port = 30000
+buff = 1024
+
+def main():
+	server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	server.bind((ipaddr, port))
+	
+	while True:
+		print (server.recv(buff))
+
+if __name__ == '__main__':
+	main()
